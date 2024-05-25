b74e23a99e4cd9ac5e33256fd8521cd3365133c6
uuufa <trump@niubi.com>
2024-05-25 20:33:55 +0800

    remove main() from namespace

--- src/template_project.cpp
+++ src/template_project.cpp
@@ -1,14 +1,11 @@
 #include <util/log.h>
 
-namespace basic_namespace
-{
 int main()
 {
-	set_default_log("template_project", spdlog::level::info, "logs/mylog.txt");
+	basic_namespace::set_default_log("template_project", spdlog::level::info, "logs/mylog.txt");
 
 	spdlog::warn("warn log");
 	spdlog::info("info log");
 
 	return 0;
 }
-} // namespace basic_namespace
