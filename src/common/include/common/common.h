f4afefc24ab09cdeaa6d53919cca7a4f7c232297
uuufa <trump@niubi.com>
2024-06-05 18:36:46 +0800

    boost::container::default_init

---
+++ src/common/include/common/common.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <memory>
+
+namespace basic_namespace
+{
+
+[[noreturn]] inline void unreachable()
+{
+#if defined(_MSC_VER) && !defined(__clang__) // MSVC
+	__assume(false);
+#else // GCC, Clang
+	__builtin_unreachable();
+#endif
+}
+
+template <typename T, typename A = std::allocator<T>>
+class default_init_allocator : public A
+{
+	typedef std::allocator_traits<A> a_t;
+
+public:
+	template <typename U>
+	struct rebind
+	{
+		using other =
+			default_init_allocator<
+				U, typename a_t::template rebind_alloc<U>>;
+	};
+
+	using A::A;
+
+	template <typename U>
+	void construct(U* ptr) noexcept(std::is_nothrow_default_constructible<U>::value)
+	{
+		::new (static_cast<void*>(ptr)) U;
+	}
+	template <typename U, typename... Args>
+	void construct(U* ptr, Args&&... args)
+	{
+		a_t::construct(static_cast<A&>(*this),
+			ptr, std::forward<Args>(args)...);
+	}
+};
+
+} // namespace basic_namespace
--- src/util/src/base64.cpp
+++ src/util/src/base64.cpp
@@ -1,42 +1,41 @@
 #include <util/base64.h>
+#include <common/common.h>
 #include <boost/beast/core/detail/base64.hpp>
+#include <boost/container/string.hpp>
 
 namespace basic_namespace
 {
+
 std::string base64_encode(std::string_view input)
 {
 	const std::size_t len = input.size();
-	std::string output;
-	output.resize(boost::beast::detail::base64::encoded_size(len));
-	output.resize(boost::beast::detail::base64::encode(output.data(), input.data(), len));
-	return output;
+	boost::container::string output(boost::beast::detail::base64::encoded_size(len), boost::container::default_init);
+	output.resize(boost::beast::detail::base64::encode(output.data(), input.data(), len), boost::container::default_init);
+	return static_cast<std::string>(output);
 }
 
 std::string base64_encode(const void* input, const size_t len)
 {
-	std::string output;
-	output.resize(boost::beast::detail::base64::encoded_size(len));
-	output.resize(boost::beast::detail::base64::encode(output.data(), input, len));
-	return output;
+	boost::container::string output(boost::beast::detail::base64::encoded_size(len), boost::container::default_init);
+	output.resize(boost::beast::detail::base64::encode(output.data(), input, len), boost::container::default_init);
+	return static_cast<std::string>(output);
 }
 
 std::string base64_decode(std::string_view input)
 {
 	const std::size_t len = input.size();
-	std::string output;
-	output.resize(boost::beast::detail::base64::decoded_size(len));
+	boost::container::string output(boost::beast::detail::base64::encoded_size(len), boost::container::default_init);
 	auto result = boost::beast::detail::base64::decode(output.data(), input.data(), len);
-	output.resize(result.first);
-	return output;
+	output.resize(result.first, boost::container::default_init);
+	return static_cast<std::string>(output);
 }
 
 std::string base64_decode(const char* input, const size_t len)
 {
-	std::string output;
-	output.resize(boost::beast::detail::base64::decoded_size(len));
+	boost::container::string output(boost::beast::detail::base64::encoded_size(len), boost::container::default_init);
 	auto result = boost::beast::detail::base64::decode(output.data(), input, len);
-	output.resize(result.first);
-	return output;
+	output.resize(result.first, boost::container::default_init);
+	return static_cast<std::string>(output);
 }
 
 } // namespace basic_namespace
