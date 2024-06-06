7d0bc9e5588d5e4af2de7b5b1c0f118c0790e72e
uuufa <trump@niubi.com>
2024-06-06 16:31:01 +0800

    qt

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -22,6 +22,12 @@ endif()
 option(BASIC_USE_MOLD "Use mold linker" OFF)
 
 option(BASIC_USE_GUI "Use qt for gui" OFF)
+if(BASIC_USE_GUI)
+    # Turn on automatic invocation of the MOC, UIC & RCC
+    set(CMAKE_AUTOMOC ON)
+    set(CMAKE_AUTOUIC ON)
+    set(CMAKE_AUTORCC ON)
+endif()
 
 if(NOT CMAKE_COLOR_DIAGNOSTICS)
     set(CMAKE_COLOR_DIAGNOSTICS ON)
