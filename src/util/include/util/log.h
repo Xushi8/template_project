fb13fbf4e6e14ed45f59781afc8a1965d0e9a7e1
uuufa <trump@niubi.com>
2024-06-11 19:56:37 +0800

    log

--- src/util/include/util/log.h
+++ src/util/include/util/log.h
@@ -5,7 +5,7 @@
 
 namespace basic_namespace
 {
-void set_default_log(std::string_view log_name, std::string_view log_path, spdlog::level::level_enum level);
+void set_default_log(std::string_view log_name, std::string_view log_path, spdlog::level::level_enum level = spdlog::level::info);
 
 void change_default_log_level(spdlog::level::level_enum level);
 } // namespace basic_namespace
