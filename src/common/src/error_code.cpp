161c3e4aac095f6c35d73b5bed525de9592d3c9d
uuufa <trump@niubi.com>
2024-07-03 12:06:24 +0800

    test

--- src/common/src/error_code.cpp
+++ src/common/src/error_code.cpp
@@ -2,13 +2,12 @@
 
 #include <fmt/core.h>
 using fmt::print;
-using fmt::println;
 
 namespace basic_namespace
 {
 void test_error_code()
 {
 	std::error_code ec = basic_namespace::error_code::file_error;
-    println("value: {}, message: {}.", ec.value(), ec.message());
+	print("value: {}, message: {}.\n", ec.value(), ec.message());
 }
-}
+} // namespace basic_namespace
