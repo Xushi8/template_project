d508afc705bb2d2fa2182290d8a9a97194201bc3
uuufa <trump@niubi.com>
2024-07-05 13:27:18 +0800

    optional

--- src/template_project.cpp
+++ src/template_project.cpp
@@ -24,23 +24,23 @@ void test_spdlog()
 
 void test_xxhash()
 {
-	u64 hash_val = hash_from_file("/media/tom/Data/test.txt");
-	print("{:x}\n", hash_val);
+	std::optional<xxh::hash_t<64>> hash_val = hash_from_file("/media/tom/Data/test.txt");
+	print("{:x}\n", *hash_val);
 	constexpr uint64_t x = 0xf74f53b60490421c;
 	constexpr uint64_t y = 0x1c429004b6534ff7;
 	print("{}\n{}\n", x, y);
 
 	std::array<int, 4> arr = {0, 0, 0, 0};
 	hash_val = xxh::xxhash3(arr);
-	print("{}\n", hash_val);
+	print("{}\n", *hash_val);
 
 	std::vector<int> vec = {0, 0, 0, 0};
 	hash_val = xxh::xxhash3(vec);
-	print("{}\n", hash_val);
+	print("{}\n", *hash_val);
 
 	std::vector<char> vec1(16, 'a');
 	hash_val = xxh::xxhash3(vec1);
-	print("{}\n", hash_val);
+	print("{}\n", *hash_val);
 }
 
 } // namespace basic_namespace
--- src/util/include/util/hash.h
+++ src/util/include/util/hash.h
@@ -8,19 +8,20 @@
 #include <string>
 #include <fstream>
 #include <array>
+#include <optional>
 
 namespace basic_namespace
 {
 static constexpr size_t hash_buffer_size = 64 * 1024;
 
 template <size_t N = 64>
-xxh::hash_t<N> hash_from_file(std::string_view file_name)
+std::optional<xxh::hash_t<N>> hash_from_file(std::string_view file_name)
 {
 	std::ifstream ifs((std::string(file_name)));
 	if (ifs.fail()) [[unlikely]]
 	{
 		perror("hash_from_file");
-		return {};
+		return std::nullopt;
 	}
 	ifs.seekg(0, std::ios::end);
 	const size_t n = static_cast<size_t>(ifs.tellg()) / sizeof(char);
