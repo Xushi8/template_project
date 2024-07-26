8b94076a32669b34020c955ec9d15a8ae8ea18b0
uuufa <trump@niubi.com>
2024-07-26 14:40:21 +0800

    spdlog

--- src/util/include/util/log.hpp
+++ src/util/include/util/log.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <string_view>
+#include <string>
 #include <spdlog/spdlog.h>
 
 namespace basic_namespace
 {
-void set_default_log(std::string_view log_name, std::string_view log_path, spdlog::level::level_enum level = spdlog::level::info);
+void set_default_log(std::string const& log_name, std::string const& log_path, spdlog::level::level_enum level = spdlog::level::info);
 
 void change_default_log_level(spdlog::level::level_enum level);
 } // namespace basic_namespace
