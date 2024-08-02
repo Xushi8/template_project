2940a5b86bc842a5f820219503c54ea61e7773cc
uuufa <trump@niubi.com>
2024-08-03 00:13:20 +0800

    qt6

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -6,9 +6,13 @@ find_package(mimalloc CONFIG REQUIRED)
 find_library(PQXX_LIB NAMES pqxx)
 find_library(PQ_LIB NAMES pq)
 
+if(BASIC_USE_GUI)
+    find_package(Qt6 REQUIRED COMPONENTS Widgets)
+endif()
+
 
 # set(Boost_USE_STATIC_LIBS ON)
-# find_package(Boost 1.81.0 REQUIRED COMPONENTS system filesystem)
+# find_package(Boost 1.81.0 REQUIRED COMPONENTS filesystem)
 
 find_path(RE2_INCLUDE_DIR NAMES re2/re2.h)
 find_library(RE2_LIBRARIES NAMES re2 libre2)
