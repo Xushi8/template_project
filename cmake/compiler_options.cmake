dfce25eb475c68fa118f3dd638702224d3ad9080
uuufa <trump@niubi.com>
2024-08-06 23:10:42 +0800

    cmake

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -9,12 +9,9 @@ endif()
 
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    # add_compile_options(-march=native)
 
-    # add_compile_options(-mavx)
     add_compile_options(-mavx2)
     add_compile_options(-mfma)
-    # add_compile_options(-march=skylake)
 
     # warnings
     add_compile_options(-Wall -Wextra -pedantic -Wshadow -Wcast-qual -Wfloat-equal -Wredundant-decls -Wundef -Wpointer-arith -Werror=return-type -Wimplicit-fallthrough)
@@ -31,16 +28,11 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang
     # add_compile_options(-fprofile-use=pgo_files)
 
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        # add_compile_options(-fsanitize=undefined)
-        # add_compile_options(-fsanitize=address)
+        add_compile_options(-fsanitize=undefined)
+        add_compile_options(-fsanitize=address)
         # add_compile_options(-fsanitize=thread)
     endif()
 
-    # add_compile_options(-fopt-info)
-
-    # add_compile_options(-save-temps=obj)
-    # add_compile_options(-masm=intel)
-    # add_compile_options(-fverbose-asm)
 endif()
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
@@ -54,11 +46,9 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang
         add_link_options(-fuse-ld=mold)
     endif()
 
-    # add_link_options(-static-libstdc++)
-
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        # add_link_options(-fsanitize=undefined)
-        # add_link_options(-fsanitize=address)
+        add_link_options(-fsanitize=undefined)
+        add_link_options(-fsanitize=address)
         # add_link_options(-fsanitize=thread)
     endif()
 
