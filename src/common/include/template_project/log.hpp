c01643311b62cb861a02e84c7f343e69541e52fe
uuufa <trump@niubi.com>
2024-08-21 22:45:52 +0800

    log

--- src/common/include/template_project/log.hpp
+++ src/common/include/template_project/log.hpp
@@ -5,7 +5,7 @@
 
 namespace basic_namespace
 {
-void set_default_log(std::string const& log_name, std::string const& log_path, spdlog::level::level_enum level = spdlog::level::info);
+void set_default_log(std::string const& log_path = "./logs/log.txt", std::string const& log_name = "basic_namespace", spdlog::level::level_enum level = spdlog::level::info);
 
 void change_default_log_level(spdlog::level::level_enum level);
 } // namespace basic_namespace
--- src/test.cpp
+++ src/test.cpp
@@ -1,143 +1,155 @@
-#include <boost/asio.hpp>
-#include <boost/unordered/unordered_flat_map.hpp>
-#include <boost/unordered/unordered_flat_set.hpp>
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_hash.hpp>
-#include <chrono>
-#include <fmt/format.h>
-#include <numeric>
-#include <random>
-#include <unordered_set>
-#include <execution>
-using namespace std::chrono_literals;
-namespace asio = boost::asio;
-using fmt::print;
+// #include <boost/asio.hpp>
+// #include <boost/unordered/unordered_flat_map.hpp>
+// #include <boost/unordered/unordered_flat_set.hpp>
+// #include <boost/uuid/uuid.hpp>
+// #include <boost/uuid/uuid_generators.hpp>
+// #include <boost/uuid/uuid_hash.hpp>
+// #include <chrono>
+// #include <fmt/format.h>
+// #include <numeric>
+// #include <random>
+// #include <unordered_set>
+// #include <execution>
+// using namespace std::chrono_literals;
+// namespace asio = boost::asio;
+// using fmt::print;
 
-template <typename F>
-std::chrono::duration<double, std::milli> time_test(F&& f)
-{
-	auto first = std::chrono::steady_clock::now();
-	f();
-	auto last = std::chrono::steady_clock::now();
-	return last - first;
-}
+// template <typename F>
+// std::chrono::duration<double, std::milli> time_test(F&& f)
+// {
+// 	auto first = std::chrono::steady_clock::now();
+// 	f();
+// 	auto last = std::chrono::steady_clock::now();
+// 	return last - first;
+// }
+
+// int main()
+// {
+// 	// std::chrono::duration<double, std::milli> tim;
+// 	// constexpr size_t n = 100000;
+// 	// tim = time_test([]()
+// 	// 	{
+// 	// 		boost::unordered_flat_set<boost::uuids::uuid> st(n);
+// 	// 		// std::unordered_set<boost::uuids::uuid> st;
+// 	// 		boost::uuids::random_generator ge;
+// 	// 		for (size_t i = 0; i < n; i++)
+// 	// 		{
+// 	// 			st.emplace(ge());
+// 	// 		}
+// 	// 	});
+
+// 	// print("{:.3f}ms\n", tim.count());
+
+// 	// tim = time_test([]()
+// 	// 	{
+// 	// 		boost::unordered_flat_set<boost::uuids::uuid> st(n);
+// 	// 		// std::unordered_set<boost::uuids::uuid> st;
+// 	// 		// boost::uuids::random_generator ge;
+// 	// 		boost::uuids::random_generator_pure ge;
+// 	// 		for (size_t i = 0; i < n; i++)
+// 	// 		{
+// 	// 			st.emplace(ge());
+// 	// 		}
+// 	// 	});
+
+// 	// print("{:.3f}ms\n", tim.count());
+
+// 	// tim = time_test([]()
+// 	// 	{
+// 	// 		boost::unordered_flat_set<boost::uuids::uuid> st(n);
+// 	// 		// std::unordered_set<boost::uuids::uuid> st;
+// 	// 		// boost::uuids::random_generator ge;
+// 	// 		// boost::uuids::random_generator_pure ge;
+// 	// 		boost::uuids::random_generator_mt19937 ge;
+// 	// 		for (size_t i = 0; i < n; i++)
+// 	// 		{
+// 	// 			st.emplace(ge());
+// 	// 		}
+// 	// 	});
+
+// 	// print("{:.3f}ms\n", tim.count());
+
+// 	// asio::io_context ctx;
+// 	// asio::steady_timer timer{ctx, 500ms};
+// 	// timer.async_wait([](std::error_code ec)
+// 	// 	{
+// 	// 		if (!ec)
+// 	// 		{
+// 	// 			print("timer 1\n");
+// 	// 		}
+// 	// 	});
+// 	// timer.async_wait([](std::error_code ec)
+// 	// 	{
+// 	// 		if (!ec)
+// 	// 		{
+// 	// 			print("timer 2\n");
+// 	// 		}
+// 	// 	});
+// 	// timer.async_wait([](std::error_code ec)
+// 	// 	{
+// 	// 		if (!ec)
+// 	// 		{
+// 	// 			print("timer 3\n");
+// 	// 		}
+// 	// 	});
+// 	// timer.async_wait([](std::error_code ec)
+// 	// 	{
+// 	// 		if (!ec)
+// 	// 		{
+// 	// 			print("timer 4\n");
+// 	// 		}
+// 	// 	});
+// 	// timer.async_wait([](std::error_code ec)
+// 	// 	{
+// 	// 		if (!ec)
+// 	// 		{
+// 	// 			print("timer 5\n");
+// 	// 		}
+// 	// 	});
+// 	// timer.cancel();
+// 	// ctx.run();
+
+// 	constexpr int n = 1000000;
+// 	std::vector<int64_t> a(n);
+// 	constexpr size_t seed = 1331;
+// 	std::mt19937_64 rng(seed);
+// 	std::generate(a.begin(), a.end(), rng);
+// 	auto b = a;
+// 	std::shuffle(a.begin(), a.end(), rng);
+// 	std::shuffle(b.begin(), b.end(), rng);
+// 	boost::unordered_flat_map<int64_t, int64_t> mp(n);
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		mp.try_emplace(a[i], b[i]);
+// 	}
+
+// 	auto query = b;
+// 	std::shuffle(query.begin(), query.end(), rng);
+// 	for (int t = 0; t < 100; t++)
+// 	{
+// 		__int128_t total = 0;
+// 		auto tim = time_test([&]
+// 			{
+// 				for (auto key : query)
+// 				{
+// 					total += mp[key];
+// 				}
+// 			});
+// 		if (t > 95)
+// 		{
+// 			print("t: {}, total: {}, elapsed: {:.3f}ms\n", t, total, tim.count());
+// 		}
+// 	}
+// }
+
+
+
+#include <boost/multiprecision/mpfr.hpp>
+#include <fmt/format.h>
+using fmt::print;
+using boost::multiprecision::mpfr_float;
 
 int main()
 {
-	// std::chrono::duration<double, std::milli> tim;
-	// constexpr size_t n = 100000;
-	// tim = time_test([]()
-	// 	{
-	// 		boost::unordered_flat_set<boost::uuids::uuid> st(n);
-	// 		// std::unordered_set<boost::uuids::uuid> st;
-	// 		boost::uuids::random_generator ge;
-	// 		for (size_t i = 0; i < n; i++)
-	// 		{
-	// 			st.emplace(ge());
-	// 		}
-	// 	});
-
-	// print("{:.3f}ms\n", tim.count());
-
-	// tim = time_test([]()
-	// 	{
-	// 		boost::unordered_flat_set<boost::uuids::uuid> st(n);
-	// 		// std::unordered_set<boost::uuids::uuid> st;
-	// 		// boost::uuids::random_generator ge;
-	// 		boost::uuids::random_generator_pure ge;
-	// 		for (size_t i = 0; i < n; i++)
-	// 		{
-	// 			st.emplace(ge());
-	// 		}
-	// 	});
-
-	// print("{:.3f}ms\n", tim.count());
-
-	// tim = time_test([]()
-	// 	{
-	// 		boost::unordered_flat_set<boost::uuids::uuid> st(n);
-	// 		// std::unordered_set<boost::uuids::uuid> st;
-	// 		// boost::uuids::random_generator ge;
-	// 		// boost::uuids::random_generator_pure ge;
-	// 		boost::uuids::random_generator_mt19937 ge;
-	// 		for (size_t i = 0; i < n; i++)
-	// 		{
-	// 			st.emplace(ge());
-	// 		}
-	// 	});
-
-	// print("{:.3f}ms\n", tim.count());
-
-	// asio::io_context ctx;
-	// asio::steady_timer timer{ctx, 500ms};
-	// timer.async_wait([](std::error_code ec)
-	// 	{
-	// 		if (!ec)
-	// 		{
-	// 			print("timer 1\n");
-	// 		}
-	// 	});
-	// timer.async_wait([](std::error_code ec)
-	// 	{
-	// 		if (!ec)
-	// 		{
-	// 			print("timer 2\n");
-	// 		}
-	// 	});
-	// timer.async_wait([](std::error_code ec)
-	// 	{
-	// 		if (!ec)
-	// 		{
-	// 			print("timer 3\n");
-	// 		}
-	// 	});
-	// timer.async_wait([](std::error_code ec)
-	// 	{
-	// 		if (!ec)
-	// 		{
-	// 			print("timer 4\n");
-	// 		}
-	// 	});
-	// timer.async_wait([](std::error_code ec)
-	// 	{
-	// 		if (!ec)
-	// 		{
-	// 			print("timer 5\n");
-	// 		}
-	// 	});
-	// timer.cancel();
-	// ctx.run();
-
-	constexpr int n = 1000000;
-	std::vector<int64_t> a(n);
-	constexpr size_t seed = 1331;
-	std::mt19937_64 rng(seed);
-	std::generate(a.begin(), a.end(), rng);
-	auto b = a;
-	std::shuffle(a.begin(), a.end(), rng);
-	std::shuffle(b.begin(), b.end(), rng);
-	boost::unordered_flat_map<int64_t, int64_t> mp(n);
-	for (int i = 0; i < n; i++)
-	{
-		mp.try_emplace(a[i], b[i]);
-	}
-
-	auto query = b;
-	std::shuffle(query.begin(), query.end(), rng);
-	for (int t = 0; t < 100; t++)
-	{
-		__int128_t total = 0;
-		auto tim = time_test([&]
-			{
-				for (auto key : query)
-				{
-					total += mp[key];
-				}
-			});
-		if (t > 95)
-		{
-			print("t: {}, total: {}, elapsed: {:.3f}ms\n", t, total, tim.count());
-		}
-	}
+	
 }
