901cc69decbabfe9aa641cb365056d2131df7e21
uuufa <trump@niubi.com>
2024-08-31 14:57:08 +0800

    link options

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -9,53 +9,55 @@ endif()
 
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-
     add_compile_options(-mavx2)
+    add_link_options(-mavx2)
     add_compile_options(-mfma)
+    add_link_options(-mfma)
 
     # warnings
     add_compile_options(-Wall -Wextra -pedantic -Wshadow -Wcast-qual -Wfloat-equal -Wredundant-decls -Wundef -Wpointer-arith -Werror=return-type -Wimplicit-fallthrough)
+    add_link_options(-Wall -Wextra -pedantic -Wshadow -Wcast-qual -Wfloat-equal -Wredundant-decls -Wundef -Wpointer-arith -Werror=return-type -Wimplicit-fallthrough)
     # add_compile_options(-Wconversion -Wsign-conversion)
+    # add_link_options(-Wconversion -Wsign-conversion)
 
     # optimize
     add_compile_options(-flto=auto)
+    add_link_options(-flto=auto)
     # add_compile_options(-fno-semantic-interposition)
+    # add_link_options(-fno-semantic-interposition)
     # add_compile_options(-fno-rtti)
+    # add_link_options(-fno-rtti)
     # add_compile_options(-ffast-math)
+    # add_link_options(-ffast-math)
 
     # PGO
     # add_compile_options(-fprofile-generate=pgo_files)
-
+    # add_link_options(-fprofile-generate=pgo_files)
     # add_compile_options(-fprofile-use=pgo_files)
+    # add_link_options(-fprofile-use=pgo_files)
 
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         add_compile_options(-fsanitize=undefined)
+        add_link_options(-fsanitize=undefined)
         # add_compile_options(-fsanitize=address)
+        # add_link_options(-fsanitize=address)
         # add_compile_options(-fsanitize=thread)
+        # add_link_options(-fsanitize=thread)
     endif()
+
+    if(BASIC_USE_MOLD)
+        add_compile_options(-fuse-ld=mold)
+        add_link_options(-fuse-ld=mold)
+    endif()
+
+    add_compile_options(-static-libstdc++)
+    add_link_options(-static-libstdc++)
 
 endif()
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     # add_compile_options(-fgraphite-identity -fdevirtualize-at-ltrans -fipa-pta -fuse-linker-plugin)
+    # add_link_options(-fgraphite-identity -fdevirtualize-at-ltrans -fipa-pta -fuse-linker-plugin)
     # add_compile_options(-floop-nest-optimize)
-endif()
-
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    if(BASIC_USE_MOLD)
-        add_link_options(-fuse-ld=mold)
-    endif()
-
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        add_link_options(-fsanitize=undefined)
-        # add_link_options(-fsanitize=address)
-        # add_link_options(-fsanitize=thread)
-    endif()
-
-    add_link_options(-static-libstdc++)
-endif()
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-
+    # add_link_options(-floop-nest-optimize)
 endif()
