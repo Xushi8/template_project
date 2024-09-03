746a24e88ac1f96b0410302ae7e41b5cd22cda74
uuufa <trump@niubi.com>
2024-09-03 16:40:31 +0800

    test

--- src/test.cpp
+++ src/test.cpp
@@ -47,85 +47,83 @@
 // 	return 0;
 // }
 
-#include <cmath>
-#include <template_project/common.hpp>
-#include <iostream>
-#include <boost/smart_ptr.hpp>
-#include <boost/smart_ptr/intrusive_ref_counter.hpp>
-using namespace std;
-using namespace boost;
+// #include <cmath>
+// #include <template_project/common.hpp>
+// #include <iostream>
+// #include <boost/smart_ptr.hpp>
+// #include <boost/smart_ptr/intrusive_ref_counter.hpp>
+// using namespace std;
+// using namespace boost;
 
-void* operator new(size_t size)
+// BASIC_BEGIN_NAMESPACE
+
+// // 自定义对象类
+// class Test;
+// ostream& operator<<(ostream& out, const Test& t);
+
+// class Test : public boost::intrusive_ref_counter<
+// 				 Test,
+// 				 boost::thread_safe_counter>
+// {
+// 	friend ostream& operator<<(ostream& out, const Test& t);
+
+// public:
+// 	Test(int d = 0) :
+// 		data(d)
+// 	{
+// 		cout << "Test()\n";
+// 	}
+
+// 	~Test()
+// 	{
+// 		cout << "~Test()\n";
+// 	}
+
+// private:
+// 	int data;
+// };
+
+// ostream& operator<<(ostream& out, const Test& t)
+// {
+// 	out << t.data;
+// 	return out;
+// }
+
+// __attribute__((noinline)) intrusive_ptr<Test> func()
+// {
+// 	Test* pt = new Test(10);
+// 	intrusive_ptr<Test> ip(pt);
+// 	cout << *ip << endl;
+// 	auto tmp = ip;
+// 	cout << *tmp << endl;
+// 	cout << "func over\n";
+// 	return tmp;
+// }
+
+// BASIC_END_NAMESPACE
+
+// #include <numbers>
+// #include <fmt/format.h>
+// using fmt::print;
+// int main()
+// {
+// 	intrusive_ptr<basic_namespace::Test> ret = basic_namespace::func();
+// 	cout << "main over\n";
+
+// 	constexpr double pi = M_PI; // 正常使用 M_PI
+// 	constexpr long double pi1 = std::numbers::pi_v<long double>;
+// 	print("{:.30f}\n{:.30f}\n", pi, pi1);
+
+// 	return 0;
+// }
+
+
+#include <compare>
+struct A
 {
-	std::cout << "Global new: Allocating " << size << " bytes\n";
-	return std::malloc(size); // 使用 std::malloc 分配内存
-}
+	int m_data;
 
-void operator delete(void* pointer) noexcept
-{
-	std::cout << "Global delete: Releasing memory\n";
-	std::free(pointer); // 使用 std::free 释放内存
-}
+	friend constexpr bool operator==(A const& lhs, A const& rhs) noexcept = default;
 
-
-BASIC_BEGIN_NAMESPACE
-
-// 自定义对象类
-class Test;
-ostream& operator<<(ostream& out, const Test& t);
-
-class Test : public boost::intrusive_ref_counter<
-				 Test,
-				 boost::thread_safe_counter>
-{
-	friend ostream& operator<<(ostream& out, const Test& t);
-
-public:
-	Test(int d = 0) :
-		data(d)
-	{
-		cout << "Test()\n";
-	}
-
-	~Test()
-	{
-		cout << "~Test()\n";
-	}
-
-private:
-	int data;
+	friend constexpr auto operator<=>(A const& lhs, A const& rhs) noexcept = default;
 };
-
-ostream& operator<<(ostream& out, const Test& t)
-{
-	out << t.data;
-	return out;
-}
-
-__attribute__((noinline)) intrusive_ptr<Test> func()
-{
-	Test* pt = new Test(10);
-	intrusive_ptr<Test> ip(pt);
-	cout << *ip << endl;
-	auto tmp = ip;
-	cout << *tmp << endl;
-	cout << "func over\n";
-	return tmp;
-}
-
-BASIC_END_NAMESPACE
-
-#include <numbers>
-#include <fmt/format.h>
-using fmt::print;
-int main()
-{
-	intrusive_ptr<basic_namespace::Test> ret = basic_namespace::func();
-	cout << "main over\n";
-
-	constexpr double pi = M_PI; // 正常使用 M_PI
-	constexpr long double pi1 = std::numbers::pi_v<long double>;
-	print("{:.30f}\n{:.30f}\n", pi, pi1);
-
-	return 0;
-}
