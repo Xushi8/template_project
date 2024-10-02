ee22131e1085a62883fd88d5b06bb2d1cbe5e641
uuufa <trump@niubi.com>
2024-10-02 23:15:13 +0800

    clang-tidy

--- src/test.cpp
+++ src/test.cpp
@@ -100,7 +100,6 @@
 // 	return tmp;
 // }
 
-
 /**
 
 *   1
@@ -149,7 +148,6 @@
 
 // 	return 0;
 // }
-
 
 /**
 single thread:
@@ -282,114 +280,15 @@ unstable use boost::block_indirect_sort
 // 	print("boost::parallel_stable_sort:\t {:.3f}ms\n", time_use.count());
 // }
 
-// #include <ratio>
-// #include <fmt/format.h>
-// using fmt::print;
-// int main()
-// {
-// 	using milli = std::ratio<1, 1000>;
-// 	print("{} {}\n", milli::num, milli::den);
-
-// 	using one = std::ratio<1, 1>;
-// 	using result = std::ratio_divide<one, milli>;
-// 	print("{} {}\n", result::num, result::den);
-// }
-
-#include <atomic>
-template <typename T>
-struct atomic_relaxed : std::atomic<T>
+#include <ratio>
+#include <fmt/format.h>
+using fmt::print;
+int main()
 {
-	static constexpr std::memory_order memory_order = std::memory_order_relaxed;
-	using std::atomic<T>::atomic;
+	using milli = std::ratio<1, 1000>;
+	print("{} {}\n", milli::num, milli::den);
 
-	atomic_relaxed(const atomic_relaxed<T>& other) noexcept :
-		std::atomic<T>(other.load(memory_order)) {}
-
-	atomic_relaxed<T>& operator=(const atomic_relaxed<T>& other) = delete;
-
-	T operator=(T val) noexcept // NOLINT(cppcoreguidelines-c-copy-assignment-signature, misc-unconventional-assign-operator)
-	{
-		store(val, memory_order);
-		return val;
-	}
-
-	operator T() const noexcept
-	{
-		return load(memory_order);
-	}
-
-	void store(T val, std::memory_order order = memory_order) noexcept
-	{
-		std::atomic<T>::store(val, order);
-	}
-
-	T load(std::memory_order order = memory_order) const noexcept
-	{
-		return std::atomic<T>::load(order);
-	}
-
-	T exchange(T val, std::memory_order order = memory_order) noexcept
-	{
-		return std::atomic<T>::exchange(val, order);
-	}
-
-	bool compare_exchange_weak(T& expected, T desired, std::memory_order order = memory_order)
-	{
-		return std::atomic<T>::compare_exchange_weak(expected, desired, order);
-	}
-
-	bool compare_exchange_strong(T& expected, T desired, std::memory_order order = memory_order)
-	{
-		return std::atomic<T>::compare_exchange_strong(expected, desired, order);
-	}
-
-	void wait(T old, std::memory_order order = memory_order)
-	{
-		std::atomic<T>::wait(old, order);
-	}
-
-	T operator&=(T val) noexcept
-	{
-		return std::atomic<T>::fetch_and(val, memory_order);
-	}
-
-	T operator|=(T val) noexcept
-	{
-		return std::atomic<T>::fetch_or(val, memory_order);
-	}
-
-	T operator^=(T val) noexcept
-	{
-		return std::atomic<T>::fetch_xor(val, memory_order);
-	}
-
-	T operator++() noexcept
-	{
-		return std::atomic<T>::fetch_add(1, memory_order) + 1;
-	}
-
-	T operator--() noexcept
-	{
-		return std::atomic<T>::fetch_sub(1, memory_order) - 1;
-	}
-
-	T operator++(int) noexcept
-	{
-		return std::atomic<T>::fetch_add(1, memory_order);
-	}
-
-	T operator--(int) noexcept
-	{
-		return std::atomic<T>::fetch_sub(1, memory_order);
-	}
-
-	T operator+=(T val) noexcept
-	{
-		return std::atomic<T>::fetch_add(val, memory_order);
-	}
-
-	T operator-=(T val) noexcept
-	{
-		return std::atomic<T>::fetch_sub(val, memory_order);
-	}
-};
+	using one = std::ratio<1, 1>;
+	using result = std::ratio_divide<one, milli>;
+	print("{} {}\n", result::num, result::den);
+}
