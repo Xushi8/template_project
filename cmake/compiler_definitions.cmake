fbe9092169d4ff83a5b32ea64dd0ce4f659c769c
uuufa <trump@niubi.com>
2024-10-08 13:20:30 +0800

    erase ozo

--- cmake/compiler_definitions.cmake
+++ cmake/compiler_definitions.cmake
@@ -6,7 +6,6 @@ endif()
 add_definitions(-DFMT_HEADER_ONLY)
 add_definitions(-DSPDLOG_FMT_EXTERNAL)
 add_definitions(-DVCL_NAMESPACE=vectorclass)
-add_definitions(-DBOOST_HANA_CONFIG_ENABLE_STRING_UDL) # ozo
 
 if(WIN32)
     add_definitions(-DNOMINMAX)
