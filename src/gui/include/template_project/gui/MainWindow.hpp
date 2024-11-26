208b68b58f468a385910ed67c8f8ff8339cd3f81
Xushi8 <111222uuufa@gmail.com>
2024-11-26 20:07:40 +0800

    qt_line

--- src/gui/include/template_project/gui/MainWindow.hpp
+++ src/gui/include/template_project/gui/MainWindow.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <template_project/common/common.hpp>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <fmt/format.h>
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QValidator>
 
 BASIC_PROJECT_BEGIN_NAMESPACE
 
@@ -9,10 +14,31 @@ class MainWindow : public QMainWindow
 {
 public:
     explicit MainWindow(QMainWindow* parent = nullptr) :
-        QMainWindow(parent)
+        QMainWindow(parent), m_line(new QLineEdit)
     {
+        this->setWindowTitle("乘法表");
         this->resize(900, 500);
+
+        auto* central_widget = new QWidget;
+        this->setCentralWidget(central_widget);
+
+        auto* layout = new QVBoxLayout;
+        central_widget->setLayout(layout);
+
+        m_line->setValidator(new QDoubleValidator);
+        layout->addWidget(m_line);
+
+        auto* button = new QPushButton;
+        button->setText("确认");
+        layout->addWidget(button);
+
+        connect(button, &QPushButton::clicked, this,
+            [this]
+            { fmt::print("button clicked, value = {}\n", m_line->text().toDouble()); });
     }
+
+private:
+    QLineEdit* m_line;
 };
 
 BASIC_PROJECT_END_NAMESPACE
--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -3,6 +3,7 @@
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QGridLayout>
+#include <QPushButton>
 #include <template_project/gui/Label.hpp>
 #include <template_project/gui/MainWindow.hpp>
 #include <fmt/format.h>
@@ -12,23 +13,6 @@ int main(int argc, char** argv)
     QApplication app(argc, argv);
 
     basic_namespace::MainWindow main_window;
-    main_window.setWindowTitle("乘法表");
-    main_window.resize(900, 500);
-
-    QWidget* central_widget = new QWidget;
-    auto* layout = new QGridLayout;
-    central_widget->setLayout(layout);
-    main_window.setCentralWidget(central_widget);
-
-    for (size_t i = 1; i <= 9; i++)
-    {
-        for (size_t j = 1; j <= 9; j++)
-        {
-            auto* label = new basic_namespace::Label;
-            label->setText(QString::fromStdString(fmt::format("{} * {} = {}", i, j, i * j)));
-            layout->addWidget(label, i - 1, j - 1);
-        }
-    }
 
     main_window.show();
 
