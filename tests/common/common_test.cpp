9a8543a098a66daaa2f59a832259a5d7042a355e
Xushi8 <111222uuufa@gmail.com>
2024-11-30 21:43:22 +0800

    gtest template

---
+++ tests/common/common_test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <template_project/common/common.hpp>
+#include <template_project/gui/gui.hpp>
+
+TEST(SampleTest, ExampleTest)
+{
+    EXPECT_EQ(1 + 1, 2);
+    // EXPECT_EQ(1, 2);
+}
