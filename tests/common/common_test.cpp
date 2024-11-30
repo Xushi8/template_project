b2477923ac5d7aaa5cd781d1300f68f6725e609f
Xushi8 <111222uuufa@gmail.com>
2024-11-30 21:50:50 +0800

    gtest template

--- tests/common/common_test.cpp
+++ tests/common/common_test.cpp
@@ -2,8 +2,27 @@
 #include <template_project/common/common.hpp>
 #include <template_project/gui/gui.hpp>
 
-TEST(SampleTest, ExampleTest)
+inline int add(int x, int y)
 {
-    EXPECT_EQ(1 + 1, 2);
-    // EXPECT_EQ(1, 2);
+    return x + y;
+}
+
+inline int sub(int x, int y)
+{
+    return x - y;
+}
+
+TEST(common, test_add)
+{
+    EXPECT_EQ(2, add(1, 1));
+}
+
+TEST(common, test_sub)
+{
+    EXPECT_EQ(0, sub(1, 1));
+}
+
+TEST(common1, test1_sub)
+{
+    EXPECT_EQ(0, sub(1, 1));
 }
--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-TEST(SampleTest, ExampleTest)
+TEST(Test_Suite_Name, Test_Name_Unique)
 {
     EXPECT_EQ(1 + 1, 2);
     // EXPECT_EQ(1, 2);
