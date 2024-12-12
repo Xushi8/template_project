8f68d9a2717bc256fb4cadb27dd0bce054576ea6
Xushi8 <111222uuufa@gmail.com>
2024-12-12 10:55:10 +0800

    from_string

--- src/common/include/template_project/common/string_convert.hpp
+++ src/common/include/template_project/common/string_convert.hpp
@@ -11,23 +11,23 @@ template <typename T>
     requires std::integral<T> || std::floating_point<T>
 constexpr std::optional<T> from_string(std::string_view s, int base = 10) noexcept
 {
-    T value;
+    T res;
     if constexpr (std::is_integral_v<T>)
     {
         // 对于整数类型，使用 base 参数
-        auto result = std::from_chars(s.data(), s.data() + s.size(), value, base);
+        auto result = std::from_chars(s.data(), s.data() + s.size(), res, base);
         if (result.ec == std::errc{}) [[likely]]
         {
-            return value;
+            return res;
         }
     }
     else if constexpr (std::is_floating_point_v<T>)
     {
         // 对于浮点类型，忽略 base 参数（std::from_chars 不支持非十进制浮点解析）
-        auto result = std::from_chars(s.data(), s.data() + s.size(), value);
+        auto result = std::from_chars(s.data(), s.data() + s.size(), res);
         if (result.ec == std::errc{} && result.ptr == s.data() + s.size()) [[likely]]
         {
-            return value;
+            return res;
         }
     }
 
