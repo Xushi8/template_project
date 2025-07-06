bbb10f6f97478bf30424ec63f4ce2bad65be339f
Xushi8 <111222uuufa@gmail.com>
2025-07-06 10:57:21 +0800

    print0

--- erase.sh
+++ erase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -f README.md
-find src/common -type d -name 'flatbuffers' | xargs rm -r
-find src/common -name '*.fbs' | xargs rm
+find src/common -type d -name 'flatbuffers' -print0 | xargs -0 rm -r
+find src/common -name '*.fbs' -print0 | xargs -0 rm
 # rm -r -f external/
